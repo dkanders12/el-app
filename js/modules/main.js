60de9873132cc91a79f9bf90f65f967f7c4a977a
dkanders12 <dkanders12@gmail.com>
2023-11-01 14:44:35 +0100

    update 3.01

--- js/modules/main.js
+++ js/modules/main.js
@@ -1,9 +1,14 @@
 let elpriser; // Opret variabel til at gemme elpriser
 
 export function getAddressByCity() {
-  return fetch(
-    `https://www.elprisenligenu.dk/api/v1/prices/2023/10-30_DK2.json`
-  )
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Måneder er nul-baserede, så tilføj 1 og pad med 0
+  const day = String(currentDate.getDate()).padStart(2, "0"); // Paddag med 0
+
+  const date = `${year}/${month}-${day}`;
+
+  return fetch(`https://www.elprisenligenu.dk/api/v1/prices/${date}_DK2.json`)
     .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -48,3 +53,54 @@ export function updateElpris() {
 }
 
 setInterval(updateClock);
+
+export function getDateAndPopulateData() {
+  const pickInput = document.querySelector("input[type='date']");
+  const perHourSection = document.getElementById("per-date");
+
+  function fetchElPriser(year, month, day) {
+    const url = `https://www.elprisenligenu.dk/api/v1/prices/${year}/${month}-${day}_DK2.json`;
+
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function populateData(ElPriser) {
+    perHourSection.innerHTML = "";
+
+    if (ElPriser && ElPriser.length > 0) {
+      ElPriser.forEach((price, index) => {
+        const divEl = document.createElement("div");
+        divEl.className = "new-date";
+        divEl.innerHTML = `<h4>${index + 1}:00</h4> <p> ${
+          price.DKK_per_kWh
+        } DKK</p>`;
+        perHourSection.appendChild(divEl);
+      });
+    } else {
+      perHourSection.textContent = "Kunne ikke hente elprisdata så langt frem";
+    }
+  }
+
+  pickInput.addEventListener("change", () => {
+    const chosenDate = pickInput.value;
+    const [year, month, day] = chosenDate.split("-");
+
+    fetchElPriser(year, month, day).then((ElPriser) => {
+      populateData(ElPriser);
+    });
+  });
+}
+
+// Kald funktionen for at starte
--- js/view.js
+++ js/view.js
@@ -1,16 +1,36 @@
-import { getAddressByCity, updateClock } from "./modules/main.js";
+import { getAddressByCity, getDateAndPopulateData } from "./modules/main.js";
 
 getAddressByCity().then((ElPriser) => {
-  const liveFeedEl = document.getElementById("live-feed-el");
-
   if (ElPriser && ElPriser.length > 0) {
     const prices = ElPriser.map((price) => price.DKK_per_kWh);
-    const sliderItems = document.querySelectorAll(".El-this-time");
+    const containers = document.querySelectorAll(".El-this-time");
 
-    prices.forEach((price, index) => {
-      sliderItems[index].textContent = `${price} DKK`;
+    containers.forEach((container, index) => {
+      const pTag = document.createElement("p");
+      pTag.textContent = `${prices[index]} DKK`;
+      container.appendChild(pTag);
     });
+    beregnMinMax(prices);
   } else {
+    const liveFeedEl = document.getElementById("live-feed-el");
     liveFeedEl.textContent = "Kunne ikke hente elprisdata.";
   }
+});
+function beregnMinMax(beloebListe) {
+  const minRing = document.getElementById("min-ring");
+  const maxRing = document.getElementById("max-ring");
+
+  if (beloebListe.length > 0) {
+    const minimum = Math.min(...beloebListe);
+    const maksimum = Math.max(...beloebListe);
+
+    minRing.textContent = ` ${minimum} DKK`;
+    maxRing.textContent = ` ${maksimum} DKK`;
+  } else {
+    minRing.textContent = "Ingen beløb tilgængelige.";
+    maxRing.textContent = "Ingen beløb tilgængelige.";
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  getDateAndPopulateData();
 });
